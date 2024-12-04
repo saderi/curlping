cd22a26b9127de2fc2909bd154839f6c0e68303e
Fatih Aytekin <fatih@aytekin.me>
2024-12-04 18:02:01 +0300

    Improve for mac

--- curlping.sh
+++ curlping.sh
@@ -9,7 +9,6 @@ if [ -z "$URL" ]; then
   exit 1
 fi
 
-
 while true; do
   if [ $C -eq 2 ]; then
     C=0
@@ -17,27 +16,28 @@ while true; do
     C=2
   fi
 
-  GREEN_COLOR="\e[$C;32m"
-  RED_COLOR="\e[$C;31m"
-  WHITE_COLOR="\e[$C;97m"
-  BLUE_COLOR="\e[$C;34m"
-  YELLOW_COLOR="\e[$C;33m"
-  RESET_COLOR="\e[;0m"
+  GREEN_COLOR="\033[$C;32m"
+  RED_COLOR="\033[$C;31m"
+  WHITE_COLOR="\033[$C;97m"
+  BLUE_COLOR="\033[$C;34m"
+  YELLOW_COLOR="\033[$C;33m"
+  RESET_COLOR="\033[0m"
 
   RESPONSE=$(curl -s -o /dev/null -w "%{http_code} %{size_download} %{time_total}" $URL)
   HTTP_CODE=$(echo $RESPONSE | awk '{print $1}')
   SIZE=$(echo $RESPONSE | awk '{print $2}')
   TIME_TOTAL=$(echo $RESPONSE | awk '{print $3}')
-  TIME=$(echo "($TIME_TOTAL * 1000)" | bc | xargs printf "%.2f \n")
-  
+  TIME=$(echo "($TIME_TOTAL * 1000)" | bc | xargs printf "%.2f")
+
   if [ "$HTTP_CODE" -eq 200 ]; then
-    STATUS_COLOR=$GREEN_COLOR # Green for 200 OK
+    STATUS_COLOR=$GREEN_COLOR
     HTTP_CODE="200 OK"
   else
-    STATUS_COLOR=$RED_COLOR # Red for other status codes
+    STATUS_COLOR=$RED_COLOR
   fi
-  
-  echo -e "${WHITE_COLOR} connected to ${YELLOW_COLOR}$URL ${WHITE_COLOR}($SIZE bytes), ${WHITE_COLOR}seq=${BLUE_COLOR}$SEQ ${WHITE_COLOR}time=${BLUE_COLOR}${TIME}${WHITE_COLOR} ms ${STATUS_COLOR} $HTTP_CODE${RESET_COLOR}"
+
+  printf "${WHITE_COLOR}connected to ${YELLOW_COLOR}%s ${WHITE_COLOR}(%d bytes), seq=${BLUE_COLOR}%-4d${WHITE_COLOR} time=${BLUE_COLOR}%-8s${WHITE_COLOR}ms ${STATUS_COLOR}%s${RESET_COLOR}\n" \
+    "$URL" "$SIZE" "$SEQ" "$TIME" "$HTTP_CODE"
   
   SEQ=$((SEQ + 1))
   sleep 1s
